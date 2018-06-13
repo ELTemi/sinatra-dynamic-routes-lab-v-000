6ba674247835c59eb0ba4d72213bab06b205ad7c
Temitope Akintoye <ruth_olukunle@yahoo.com>
2018-06-13 20:25:57 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -14,8 +14,6 @@ class App < Sinatra::Base
   get "/say/:number/:phrase" do
     @num = params[:number].to_i
     @phrase = params[:phrase].scan(/\w+/)
-    @num.times do
-      puts "#{@phrase.join(" ")}"
-    end
+    @num * "#{@phrase.join(" ")}"
   end
 end
