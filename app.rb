2fc4e226f1088d8f7be982b3c39344d19aabc903
Temitope Akintoye <ruth_olukunle@yahoo.com>
2018-06-13 20:28:56 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -14,6 +14,6 @@ class App < Sinatra::Base
   get "/say/:number/:phrase" do
     @num = params[:number].to_i
     @phrase = params[:phrase].split("%20")
-    @num * "#{@phrase.join(" ")}"
+     "#{@phrase.join(" ")}"
   end
 end
